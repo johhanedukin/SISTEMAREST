1cdbc0c400292074f115fbfb135a1044205c7e7a
johhanedukin <johhanedukin@gmail.com>
2019-07-08 11:39:39 -0500

    seeders faltal

---
+++ database/seeds/TablaPermisoSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TablaPermisoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Permiso::class)->create();
+            
+    }
+}
